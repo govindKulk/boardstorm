ac91c18ebc7ef4b5dabc36fbf6459c3f01db34e0
govindKulk <kulkarnigovind2003@gmail.com>
2025-01-22 19:58:27 +0530

    set up folder structure

---
+++ src/app/boards/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SingleBoardPage() {
+  return (
+    <div>All Board Page</div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SingUpPage() {
+  return (
+    <div>sing up page</div>
+  )
+}
